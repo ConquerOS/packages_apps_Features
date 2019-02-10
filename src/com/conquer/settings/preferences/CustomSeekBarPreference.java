14a49283d9903533f392c99702b804cf0f1d4636
Pranav Vashi <neobuddy89@gmail.com>
2019-02-11 02:03:16 +0530

    Features: Add Custom Seekbar preferences
    Signed-off-by: HeroBuxx <herobuxx@conqueros.co>

--- src/com/conquer/settings/preferences/CustomSeekBarPreference.java
+++ src/com/conquer/settings/preferences/CustomSeekBarPreference.java
@@ -1,19 +1,17 @@
-/*
- * Copyright (C) 2016 The Dirty Unicorns Project
+/* * Copyright (C) 2017 ColtOS Android
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License
+ * limitations under the License.
  */
-
 package com.conquer.settings.preferences;
 
 import android.content.Context;
@@ -26,7 +24,7 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import androidx.preference.*;
 
-import com.conquer.settings.R;
+import com.android.settings.R;
 
 public class CustomSeekBarPreference extends Preference implements SeekBar.OnSeekBarChangeListener {
     private final String TAG = getClass().getName();
@@ -38,7 +36,7 @@ public class CustomSeekBarPreference extends Preference implements SeekBar.OnSee
     private int mInterval = 1;
     private int mCurrentValue;
     private int mDefaultValue = -1;
-    private int mMax = 100;
+    private int mMax = 255;
     private String mUnits = "";
     private String mDefaultText = "";
     private SeekBar mSeekBar;
@@ -51,12 +49,9 @@ public class CustomSeekBarPreference extends Preference implements SeekBar.OnSee
         final TypedArray a = context.obtainStyledAttributes(
                 attrs, R.styleable.CustomSeekBarPreference);
 
-        mMax = attrs.getAttributeIntValue(ANDROIDNS, "max", 100);
+        mMax = attrs.getAttributeIntValue(ANDROIDNS, "max", 255);
         mMin = attrs.getAttributeIntValue(SETTINGS_NS, "min", 0);
         mDefaultValue = attrs.getAttributeIntValue(ANDROIDNS, "defaultValue", -1);
-        if (mDefaultValue > mMax) {
-            mDefaultValue = mMax;
-        }
         mUnits = getAttributeStringValue(attrs, SETTINGS_NS, "units", "");
         mDefaultText = getAttributeStringValue(attrs, SETTINGS_NS, "defaultText", "Def");
 
@@ -81,7 +76,7 @@ public class CustomSeekBarPreference extends Preference implements SeekBar.OnSee
         mSeekBar = new SeekBar(context, attrs);
         mSeekBar.setMax(mMax - mMin);
         mSeekBar.setOnSeekBarChangeListener(this);
-        setLayoutResource(R.layout.preference_conquer_seekbar);
+        setLayoutResource(R.layout.preference_custom_seekbar);
     }
 
     public CustomSeekBarPreference(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -148,10 +143,9 @@ public class CustomSeekBarPreference extends Preference implements SeekBar.OnSee
         mSeekBar.setProgress(mCurrentValue - mMin);
         mTitle = (TextView) view.findViewById(android.R.id.title);
 
+        mSeekBar.setEnabled(isEnabled());
         view.setDividerAllowedAbove(false);
         //view.setDividerAllowedBelow(false);
-
-        mSeekBar.setEnabled(isEnabled());
     }
 
     public void setMax(int max) {
@@ -236,7 +230,7 @@ public class CustomSeekBarPreference extends Preference implements SeekBar.OnSee
         if (mDefaultValue > mMax) {
             mDefaultValue = mMax;
         }
-        if (mCurrentValue == mDefaultValue) {
+        if (mCurrentValue == mDefaultValue && mStatusText != null) {
             mStatusText.setText(mDefaultText);
         }
     }
